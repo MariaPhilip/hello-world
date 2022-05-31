000c454d4fc19577f7fab961eea27ac0ac7b3b27
Maria <maria.anna.philip@gmail.com>
2022-05-31 09:42:30 +0530

    Add

--- src/com/maria/test/Hello2.java
+++ src/com/maria/test/Hello2.java
@@ -2,8 +2,14 @@ package com.maria.test;
 
 public class Hello2 
 {
+	int c;
 	public Hello2() 
 	{
 	 System.out.println("Hello This is maria");
+	}
+	Hello2( int a, int b) 
+	{
+		c= a+b;
+	 System.out.println("Additions = "+c);
 	}
 }
--- src/com/maria/test/HelloWorld.java
+++ src/com/maria/test/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) 
 	{
 		System.out.print("Hello Worlds");
-		Hello2 h=new Hello2();
+		Hello2 h=new Hello2(4,5);
+		
 
 	}
 
