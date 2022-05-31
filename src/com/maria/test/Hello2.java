adeed40adc2007cc84ed90095705ceb4aef27bd6
Maria <maria.anna.philip@gmail.com>
2022-05-31 09:29:56 +0530

    Second commit

---
+++ src/com/maria/test/Hello2.java
@@ -0,0 +1,9 @@
+package com.maria.test;
+
+public class Hello2 
+{
+	public Hello2() 
+	{
+	 System.out.println("Hello This is maria");
+	}
+}
--- src/com/maria/test/HelloWorld.java
+++ src/com/maria/test/HelloWorld.java
@@ -2,8 +2,10 @@ package com.maria.test;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.print("Hello Worlds");
+		Hello2 h=new Hello2();
 
 	}
 
